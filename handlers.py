a2f1ff2ace0daa1be8946497f43d7632ce20765e
Dmitros Camenev <dmit.cameneff@yandex.ru>
2024-04-22 07:08:20 +0300

    remove code for  root execution

--- handlers.py
+++ handlers.py
@@ -8,6 +8,7 @@ import os
 import asyncio
 
 from app_text import SYSTEM_REBOOT_CMD
+from app_text import HELLO_MSG, REBOOT_MSG, STATUS_ANSWER_MSG #, REBOOT_NO_MSG
 
 
 router = Router()
@@ -15,13 +16,13 @@ router = Router()
 @router.message(Command("start"))
 async def start_handler(msg: Message):
     if msg.from_user.id == USER_TLG_ID:
-        await msg.answer("Привет! Добро пожаловать в бот-помощник для обслуживания точки доступа к 'МИС Барс'.")
+        await msg.answer(HELLO_MSG)
 
 
 @router.message(Command("status"))
 async def start_handler(msg: Message):
     if msg.from_user.id == USER_TLG_ID:
-        await msg.answer('Отправляем данные о параметрах точки доступа к МИС "Барс".')
+        await msg.answer(STATUS_ANSWER_MSG)
         await asyncio.sleep(10)
         await msg.answer('Данные ...')
 
@@ -29,11 +30,5 @@ async def start_handler(msg: Message):
 @router.message(Command("reboot"))
 async def start_handler(msg: Message):
     if msg.from_user.id == USER_TLG_ID:
-        await msg.answer('Отправляем сигнал на перезагрузку точки доступа МИС "Барс".')
+        await msg.answer(REBOOT_MSG)
         os.system(SYSTEM_REBOOT_CMD)
-        
-
-
-# @router.message()
-# async def message_handler(msg: Message):
-#     await msg.answer(f"Твой ID: {msg.from_user.id}")
