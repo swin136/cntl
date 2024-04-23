dabdb1d9b9587e22082e67ce9091c5ba91fd2da7
Dmitros Camenev <dmit.cameneff@yandex.ru>
2024-04-23 08:36:36 +0300

    add commands for control network interface

--- app_text.py
+++ app_text.py
@@ -11,6 +11,12 @@ NO_TEMP_FOUND = "Температура: <I>нет данных</I>"
 # Команда оболочки для перезагрузки системы 
 SYSTEM_REBOOT_CMD = '/sbin/reboot'
 
+# Команды оболочки для включения/отключения интерфейса
+# ВАЖНО! Требуются изменения в файле /etc/sudoerrs (я внес такие)
+# orangepi  ALL=NOPASSWD: /sbin/ip
+SYSTEM_UP_NETWORK_CMD = 'sudo /sbin/ip link set eth0 up'
+SYSTEM_DOWN_NETWORK_CMD = 'sudo /sbin/ip link set eth0 down'
+
 
 # Имена фалов из каталога /proc для получения данных о системе
 # Использование памяти
@@ -19,3 +25,5 @@ MEM_INFO_FILE = '/proc/meminfo'
 THERMAL_FILE =  '/sys/devices/virtual/thermal/thermal_zone0/temp'
 # Время непрерывной работы
 UPTIME_INFO_FILE = '/proc/uptime'
+# файл для получения статуса интерфейса eth0
+NET_DEVICE_CARRIER_FILE =  '/sys/class/net/eth0/carrier'
