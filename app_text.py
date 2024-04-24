ed4398264aba96033c9a50bfbcf4a1cd9f11dc07
Dmitros Camenev <dmit.cameneff@yandex.ru>
2024-04-24 07:08:46 +0300

    add command for change host routing

--- app_text.py
+++ app_text.py
@@ -1,6 +1,6 @@
 # Текстовые сообщения, передаваемые через Telegram-бота 
-HELLO_MSG = "Привет! Добро пожаловать в бот-помощник для обслуживания точки доступа к 'МИС Барс'."
-REBOOT_MSG = 'Отправляем сигнал на перезагрузку точки доступа к <b>МИС "Барс"</b>.'
+HELLO_MSG = "Привет! Добро пожаловать в бот-помощник 'МИС Барс'. Ваше действие:"
+REBOOT_MSG = 'Отправлен сигнал на перезагрузку точки доступа к <b>МИС "Барс"</b>.'
 LINK_OFF_MSG = 'Выключаем связь с сетью <b>МИС "Барс"</b>.'
 LINK_ON_MSG = 'Включаем связь с сетью <b>МИС "Барс"</b>.'
 #REBOOT_NO_MSG = 'У бота нет прав для перезагрузки точки доступа к МИС "Барс".'
@@ -10,6 +10,12 @@ AFTER_REBOOT_MSG = 'Бот на устройстве <b><I><u>Бегемотик
 
 NO_TEMP_FOUND = "Температура: <I>нет данных</I>"
 
+# Кнопки меню бота
+STATUS_BUTTON = 'Статус устройства'
+REBOOT_BUTTON = 'Перегрузить устройство'
+CONNECT_TO_BARS = 'Подключиться к "Барсу"'
+DISCONNET_FROM_BARS = 'Отключиться от "Барса"'
+
 # Команда оболочки для перезагрузки системы 
 SYSTEM_REBOOT_CMD = '/sbin/reboot'
 
@@ -18,6 +24,11 @@ SYSTEM_REBOOT_CMD = '/sbin/reboot'
 # orangepi  ALL=NOPASSWD: /sbin/ip
 SYSTEM_UP_NETWORK_CMD = 'sudo /sbin/ip link set eth0 up'
 SYSTEM_DOWN_NETWORK_CMD = 'sudo /sbin/ip link set eth0 down'
+
+TARGET = '100.117.0.1'
+GTW = '192.168.1.1'
+
+RESTORE_ROUTE = f'sudo /sbin/ip route add {TARGET} via {GTW}'
 
 
 # Имена фалов из каталога /proc для получения данных о системе
