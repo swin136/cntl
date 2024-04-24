8f2677b47c6b5f44c6250be9a972dbf8a3a6d96a
Dmitros Camenev <dmit.cameneff@yandex.ru>
2024-04-24 07:05:12 +0300

    add keyboard to bot

---
+++ kb.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+from app_text import STATUS_BUTTON, REBOOT_BUTTON, CONNECT_TO_BARS, DISCONNET_FROM_BARS
+
+
+kb = [
+        [KeyboardButton(text= CONNECT_TO_BARS), KeyboardButton(text=STATUS_BUTTON)],
+        [KeyboardButton(text=DISCONNET_FROM_BARS), KeyboardButton(text=REBOOT_BUTTON)],
+        
+    ]
+
+keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True,
+        input_field_placeholder="Или введите команду для бота")
