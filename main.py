413a3b010997af2eb319f46d53b60ff3b6fb19e6
Dmitros Camenev <dmit.cameneff@yandex.ru>
2024-04-25 14:29:15 +0300

    fix little bugs

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def main():
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-   
+    
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
