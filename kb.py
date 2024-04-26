2ea0889147473f40dc6320edd474d9c01352810f
Dmitri Camenev <dmit.cameneff@yandex.ru>
2024-04-26 10:38:18 +0300

    change REBOOT_BUTTON to HELP_BUTTON in bot's menu

--- kb.py
+++ kb.py
@@ -1,11 +1,11 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
-from app_text import STATUS_BUTTON, REBOOT_BUTTON, CONNECT_TO_BARS, DISCONNET_FROM_BARS
+from app_text import STATUS_BUTTON, HELP_BUTTON, CONNECT_TO_BARS, DISCONNET_FROM_BARS
 
 
 kb = [
         [KeyboardButton(text= CONNECT_TO_BARS), KeyboardButton(text=STATUS_BUTTON)],
-        [KeyboardButton(text=DISCONNET_FROM_BARS), KeyboardButton(text=REBOOT_BUTTON)],
+        [KeyboardButton(text=DISCONNET_FROM_BARS), KeyboardButton(text=HELP_BUTTON)],
         
     ]
 
