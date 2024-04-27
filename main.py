59ee349620c92f14afd150ac20478d7805ac17a9
Dmitri Camenev <dmit.cameneff@yandex.ru>
2024-04-27 15:11:10 +0300

    add create file during first run after power ON/OFF

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
@@ -12,17 +13,28 @@ from handlers import router
 from config import BOT_TOKEN
 from config import USER_TLG_IDS
 
-from app_text import AFTER_REBOOT_MSG
+from app_text import AFTER_REBOOT_MSG, AFTER_POWER_ON_OFF_MSG
+from app_text import OS_SEMAPHOR_CREATE, FIRST_LAUNCH_FILE 
 
 
 async def main():
     # старая версия - на эту строчку вываливается Warning
     # bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    # Работа с файлом-семафором
+    first_run = True
+    if not os.path.isfile(FIRST_LAUNCH_FILE):
+        process = await asyncio.create_subprocess_shell(OS_SEMAPHOR_CREATE)
+        await process.communicate()
+        first_run = False
+
     # Отправляем приветсвенное сообщение о начале работы бота
     for item in USER_TLG_IDS:
         try:
             await bot.send_message(item, AFTER_REBOOT_MSG)
+            if not first_run:
+                # Оповещаем о первом запуске платы
+                await bot.send_message(item, AFTER_POWER_ON_OFF_MSG)
         except TelegramBadRequest:
             continue
         
